ee1ccf69b3abe654c3bb32c2fb6fce7c5c86155e
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-04-27 19:41:46 -0700

    gets box a

--- scraper.js
+++ scraper.js
@@ -9,57 +9,6 @@ var cheerioTableparser = require('cheerio-tableparser');
 var fs = require('fs');
 require('console.table');
 
-var matches = [
-  ['0', 'a', 'b', 'c', 'd', 'e', 'f'],
-  ['a', '0', '1', '2', '3', '4', '5' ],
-  ['b', '1', '0', '1', '2', '3', '4' ],
-  ['c', '2', '1', '0', '1', '2', '3' ],
-  ['d', '3', '2', '1', '0', '1', '2' ],
-  ['e', '4', '3', '2', '1', '0', '1' ],
-  ['f', '5', '4', '3', '2', '1', '0' ]
-]
-
-// var data = [ [ 'X', '2', '1', '2', '3', '1' ],
-//   [ '5', 'X', '4', '1', '1', '1' ],
-//   [ '6', '3', 'X', '0', '', '' ],
-//   [ '5', '6', '5', 'X', '5', '5' ],
-//   [ '4', '6', '', '0', 'X', '' ],
-//   [ '6', '6', '', '0', '', 'X' ] ]
-
-var fullTable = [ [ '',
-    'Colin Grant',
-    'Eric Katerman',
-    'Sam Sternberg',
-    'Ashley Kayler',
-    'Shrinu Kushagra',
-    'Sam McCormick' ],
-  [ 'CG', 'X', '2', '1', '2', '3', '1' ],
-  [ 'EK', '5', 'X', '4', '1', '1', '1' ],
-  [ 'SS', '6', '3', 'X', '0', '', '' ],
-  [ 'AK', '5', '6', '5', 'X', '5', '5' ],
-  [ 'SK', '4', '6', '', '0', 'X', '' ],
-  [ 'SM', '6', '6', '', '0', '', 'X' ],
-  [ 'Total', '26', '23', '10', '3', '9', '7' ] ]
-
-var opponentOutput = [
-  ['a', 'b'],
-  ['a', 'c'],
-  ['a', 'd'],
-  ['a', 'e'],
-  ['a', 'f'],
-  ['b', 'c'],
-  ['b', 'd'],
-  ['b', 'e'],
-  ['b', 'f'],
-  ['c', 'd'],
-  ['c', 'e'],
-  ['c', 'f'],
-  ['d', 'e'],
-  ['d', 'f'],
-  ['e', 'f']
-]
-
-
 
 var parseMatchScores = (array) => {
   var arr = JSON.parse(JSON.stringify(array));
@@ -100,41 +49,75 @@ var joinOpponentsAndScores = (matchUps, scores) => {
   })
 }
 
-
-
-// console.log('matches: ', fullTable)
-// var scores = parseMatchScores(fullTable)
-// console.log('scores: ', scores);
-// var matchUps = parseMatchOpponents(fullTable)
-// console.log('matchUps: ', matchUps);
-// console.log(JSON.stringify(parseMatchOpponents(fullTable)) === JSON.stringify(opponentOutput));
-// var games = scores.map((score, i) => {
-//   return matchUps[i].concat(score)
-// })
-// console.log('games', games);
-
-
-
-
 var fetchMatchData = () => {
 
   request('http://www.calsquash.com/boxleague/s4.php?file=current.players', (err, success, body) => {
     var $ = cheerio.load(body)
+
     cheerioTableparser($);
     var table = $('table').first().parsetable(false, false, true).slice(1, -2);
-    console.log('table', table)
-    // var justTheScores = table.slice(1).map(row => row.slice(1))
-    // data.slice(1).forEach(row => data1.unshift(row[0]))
-    // justTheScores.pop();
-    // console.log('justTheScores', justTheScores);
-    var matches = joinOpponentsAndScores(parseMatchOpponents(table), parseMatchScores(table));
-    console.log('matches: ', matches)
+    console.log('table: ', table)
+    // $('table').each(() => {
+      // console.log($(this).first().parsetable(false, false, false))
+    // })
+    // var matches = joinOpponentsAndScores(parseMatchOpponents(table), parseMatchScores(table));
+    // console.log('matches: ', matches)
 
 
   })
 }
 
 fetchMatchData()
+
+// var opponentOutput = [
+//   ['a', 'b'],
+//   ['a', 'c'],
+//   ['a', 'd'],
+//   ['a', 'e'],
+//   ['a', 'f'],
+//   ['b', 'c'],
+//   ['b', 'd'],
+//   ['b', 'e'],
+//   ['b', 'f'],
+//   ['c', 'd'],
+//   ['c', 'e'],
+//   ['c', 'f'],
+//   ['d', 'e'],
+//   ['d', 'f'],
+//   ['e', 'f']
+// ]
+
+// var matches = [
+//   ['0', 'a', 'b', 'c', 'd', 'e', 'f'],
+//   ['a', '0', '1', '2', '3', '4', '5' ],
+//   ['b', '1', '0', '1', '2', '3', '4' ],
+//   ['c', '2', '1', '0', '1', '2', '3' ],
+//   ['d', '3', '2', '1', '0', '1', '2' ],
+//   ['e', '4', '3', '2', '1', '0', '1' ],
+//   ['f', '5', '4', '3', '2', '1', '0' ]
+// ]
+
+// var data = [ [ 'X', '2', '1', '2', '3', '1' ],
+//   [ '5', 'X', '4', '1', '1', '1' ],
+//   [ '6', '3', 'X', '0', '', '' ],
+//   [ '5', '6', '5', 'X', '5', '5' ],
+//   [ '4', '6', '', '0', 'X', '' ],
+//   [ '6', '6', '', '0', '', 'X' ] ]
+
+// var fullTable = [ [ '',
+//     'Colin Grant',
+//     'Eric Katerman',
+//     'Sam Sternberg',
+//     'Ashley Kayler',
+//     'Shrinu Kushagra',
+//     'Sam McCormick' ],
+//   [ 'CG', 'X', '2', '1', '2', '3', '1' ],
+//   [ 'EK', '5', 'X', '4', '1', '1', '1' ],
+//   [ 'SS', '6', '3', 'X', '0', '', '' ],
+//   [ 'AK', '5', '6', '5', 'X', '5', '5' ],
+//   [ 'SK', '4', '6', '', '0', 'X', '' ],
+//   [ 'SM', '6', '6', '', '0', '', 'X' ],
+//   [ 'Total', '26', '23', '10', '3', '9', '7' ] ]
 
 // var opponentOutput = [
 //   ['a', 'b'],
